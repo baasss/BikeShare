5802bcf28f014f2bdf2045c3f3733a589cd5141e
Apoorva Gupta <gupta08.apoorva@gmail.com>
2014-11-14 01:56:08 -0800

    Updated for user registration

--- src/main/java/com/baasss/common/model/User.java
+++ src/main/java/com/baasss/common/model/User.java
@@ -7,6 +7,71 @@ public class User {
 	
 	private String phonenumber;
 	
+	public String name;
+	public String email;
+	public String username;
+	public String password;
+	public String mobileNo;
+	public String bikes_owned;
+	
+	public String getBikes_owned() {
+		return bikes_owned;
+	}
+	public void setBikes_owned(String owned) {
+		bikes_owned = owned;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String n) {
+		name = n;
+	/*	System.out.println("name n:");
+		System.out.println(n);
+		System.out.println("name:");
+		System.out.println(name);
+		System.out.printf("set name::",name);
+		System.out.println();
+		*/
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String e) {
+		
+		email = e;
+	/*	System.out.println("name e:");
+		System.out.println(e);
+		System.out.println("email:");
+		System.out.println(email);
+		System.out.printf("set email::",email);
+		System.out.println();
+		System.out.printf("this.email======",email);
+		System.out.println();
+		*/
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String uname) {
+	username = uname;
+		//System.out.printf("set username::",username);
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String pwd) {
+		password = pwd;
+		// System.out.printf("set password::",password);
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mNo) {
+		mobileNo = mNo;
+	}
+
+	
+	
 	public String getPhonenumber() {
 		return phonenumber;
 	}
